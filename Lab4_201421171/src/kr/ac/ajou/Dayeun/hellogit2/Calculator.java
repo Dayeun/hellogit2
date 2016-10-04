2840d36b39a7afa11cf7be3fc0670f0f3824f2c4
jek90 <eunkyung900@gmail.com>
2016-10-04 11:48:18 +0900

    as

--- Lab4_201421171/src/kr/ac/ajou/Dayeun/hellogit2/Calculator.java
+++ Lab4_201421171/src/kr/ac/ajou/Dayeun/hellogit2/Calculator.java
@@ -34,7 +34,7 @@ public class Calculator {
 	}
 	public String toString(){
 		//programmer A
-		// hallo
+		// hallo1
 		String tmb;
 		
 		tmb=("name: Dayeun,kangbada0728,jek90"+"StudentId: 201421171,201220987,201222777"+"Repository URI: https://github.com/Dayeun/hellogit2.git");
