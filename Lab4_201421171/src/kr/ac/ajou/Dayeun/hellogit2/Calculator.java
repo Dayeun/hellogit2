623c47e394912c47d500481e358a8a17669ff9c4
ajou <ajou@ajou-PC>
2016-10-04 11:07:31 +0900

    Programmer

--- Lab4_201421171/src/kr/ac/ajou/Dayeun/hellogit2/Calculator.java
+++ Lab4_201421171/src/kr/ac/ajou/Dayeun/hellogit2/Calculator.java
@@ -9,7 +9,10 @@ public class Calculator {
 	}
 	
 	public double add(double x, double y){
-		return 0;
+		//programmer A
+				
+		
+		return x+y;
 	}
 	
 	public double subract(double x, double y){
@@ -21,15 +24,21 @@ public class Calculator {
 	}
 	
 	public double multiply(double x, double y){
-		return 0;
+		//programmer A
+		
+		return x*y;
 	}
 	
 	public int remainder(double x, double y){
 		return 0;
 	}
 	public String toString(){
+		//programmer A
+		String tmb;
 		
-		return null;
+		tmb=("name: Dayeun,kangbada0728,jek90"+"StudentId: 201421171,201220987,201222777"+"Repository URI: https://github.com/Dayeun/hellogit2.git")
+		
+		return tmb;
 	}
 
 }
