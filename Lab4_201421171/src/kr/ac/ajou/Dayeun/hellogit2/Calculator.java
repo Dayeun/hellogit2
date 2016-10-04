dbfaee8e8cf1ae03d48c1e38f2c13985a9a1002d
DongHunKang <DongHunKang@172.30.119.10>
2016-10-04 11:13:02 +0900

    donghun

--- Lab4_201421171/src/kr/ac/ajou/Dayeun/hellogit2/Calculator.java
+++ Lab4_201421171/src/kr/ac/ajou/Dayeun/hellogit2/Calculator.java
@@ -12,12 +12,17 @@ public class Calculator {
 		return 0;
 	}
 	
-	public double subract(double x, double y){
-		return 0;
+	public double subtract(double x, double y){
+		double z=x-y;
+		return z;
 	}
 	
 	public double divide(double x, double y){
-		return 0;
+		if(y==0)
+			System.out.println("divide Error!");
+		else
+			double z=x/y;
+		return z;//
 	}
 	
 	public double multiply(double x, double y){
@@ -25,7 +30,8 @@ public class Calculator {
 	}
 	
 	public int remainder(double x, double y){
-		return 0;
+		double z=x%y;
+		return z;
 	}
 	public String toString(){
 		
