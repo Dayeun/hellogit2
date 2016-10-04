29696b01d0240e10373c625150cb28811353ab26
Dayeun <turtleneck@ajou.ac.kr>
2016-10-04 10:39:50 +0900

    committed Hellogit2 project

---
+++ Lab4_201421171/src/kr/ac/ajou/Dayeun/hellogit2/Calculator.java
@@ -0,0 +1,35 @@
+package kr.ac.ajou.Dayeun.hellogit2;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	
+	public double add(double x, double y){
+		return 0;
+	}
+	
+	public double subract(double x, double y){
+		return 0;
+	}
+	
+	public double divide(double x, double y){
+		return 0;
+	}
+	
+	public double multiply(double x, double y){
+		return 0;
+	}
+	
+	public int remainder(double x, double y){
+		return 0;
+	}
+	public String toString(){
+		
+		return null;
+	}
+
+}
